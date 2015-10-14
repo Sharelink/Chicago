c865972224fbfb51410c16d64593a14c6aa54a96
AlexChow <cplover@live.com>
2015-10-15 01:57:16 +0800

    initial commit

---
+++ src/Chicago/Extension/NotificaionCenterExtension.cs
@@ -0,0 +1,73 @@
+﻿using CSharpServerFramework.Extension;
+using CSharpServerFramework;
+using CSServerJsonProtocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chicago.Extension
+{
+    [ExtensionInfo("NotificationCenter")]
+    public class NotificaionCenterExtension : JsonExtensionBase
+    {
+        public static NotificaionCenterExtension Instance { get; private set; }
+        private IDictionary<string, ICSharpServerSession> subscriptionMap;
+        public void Subscript(string userId, ICSharpServerSession session)
+        {
+            using (var subscription = ChicagoServer.MessagePubSubServerClientManager.GetClient().CreateSubscription())
+            {
+                subscription.OnUnSubscribe = channel =>
+                {
+                    subscriptionMap.Remove(userId);
+                };
+
+                subscription.OnSubscribe = channel =>
+                {
+                    subscriptionMap[userId] = session;
+                };
+
+                subscription.OnMessage = (channel, message) =>
+                {
+                    var ss = subscriptionMap[channel];
+                    if (ss != null)
+                    {
+                        Log("Notified");
+                        this.SendJsonResponse(ss, new { ChatId = message }, ExtensionName, "UsrNewMsg");
+                    }
+                };
+                subscription.SubscribeToChannels(userId);
+            };
+        }
+
+        public override void Init()
+        {
+            subscriptionMap = new Dictionary<string, ICSharpServerSession>();
+            ChicagoServer.Instance.OnSessionDisconnected += Instance_OnSessionDisconnected;
+            Instance = this;
+        }
+
+        private void Instance_OnSessionDisconnected(object sender, CSServerEventArgs e)
+        {
+            var session = e.State as ICSharpServerSession;
+            if (session != null)
+            {
+                var sharelinker = session.User as Sharelinker;
+                if (sharelinker != null)
+                {
+                    using (var subscription = ChicagoServer.MessagePubSubServerClientManager.GetClient().CreateSubscription())
+                    {
+                        subscriptionMap.Remove(sharelinker.UserData.UserId);
+                        subscription.UnSubscribeFromChannels(sharelinker.UserData.UserId);
+                    }
+                }
+            }
+        }
+
+        [CommandInfo(1, "UsrNewMsg")]
+        public void NotifyUserNewMessage(ICSharpServerSession session, dynamic msg)
+        {
+            this.SendJsonResponse(session, new { ChatId = "" }, ExtensionName, "UsrNewMsg");
+        }
+    }
+}
---
+++ src/ChicagoClientLib/ChicagoClient.cs
@@ -0,0 +1,106 @@
+﻿using CSharpClientFramework;
+using CSharpClientFramework.Client;
+using CSServerJsonProtocol;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ChicagoClientLib
+{
+    public class ChicagoClient : CSharpServerClientBase
+    {
+        public ChicagoClient() : base(new JsonMessageDeserializer())
+        {
+            
+        }
+
+        public void Validate(string appkey,string appInstanceId)
+        {
+            if (IsRunning)
+            {
+                SendJsonMessage("BahamutAppValidation", "Login", new
+                {
+                    Appkey = appkey,
+                    AppInstanceId = appInstanceId
+                });
+            }
+            else
+            {
+                throw new CSharpClientException("Client Not Running,Invoke Start Before Validate");
+            }
+        }
+
+        public void AddValidateReturnHandler(EventHandler<CSharpServerClientEventArgs> Callback)
+        {
+            AddHandlerCallback("BahamutAppValidation", "Login", Callback);
+        }
+
+        public void SendJsonMessageAsync(string Extension, int CommandId, object Message)
+        {
+            var msgBytes = JsonProtocolUtil.SerializeMessage(Extension, CommandId, Message);
+            SendMessageAsync(msgBytes, msgBytes.Length);
+        }
+
+        public void SendJsonMessage(string Extension, int CommandId, object Message)
+        {
+            var msgBytes = JsonProtocolUtil.SerializeMessage(Extension, CommandId, Message);
+            SendMessage(msgBytes, msgBytes.Length);
+        }
+
+        public void SendJsonMessageAsync(string Extension, string CommandName, object Message)
+        {
+            var msgBytes = JsonProtocolUtil.SerializeMessage(Extension, CommandName, Message);
+            SendMessageAsync(msgBytes, msgBytes.Length);
+        }
+
+        public void SendJsonMessage(string Extension,string CommandName,object Message)
+        {
+            var msgBytes = JsonProtocolUtil.SerializeMessage(Extension, CommandName, Message);
+            SendMessage(msgBytes, msgBytes.Length);
+        }
+
+        public void Start(IPAddress iPAddress, object chicagoServerPort)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class JsonMessageDeserializer : IDeserializeMessage
+    {
+        public CSharpServerClientBaseMessage GetMessageFromBuffer(byte[] Buffer, int len)
+        {
+            var route = JsonProtocolUtil.DeserializeRoute(Buffer, len);
+            var returnObj = JsonProtocolUtil.DeserializeMessage(0, Buffer, len);
+            var jsonMsg = new JsonMessage()
+            {
+                CommandId = route.CmdId,
+                Extension = route.ExtName,
+                CommandName = route.CmdName,
+                Result = returnObj
+            };
+            return jsonMsg;
+        }
+    }
+
+
+    [Serializable]
+    public class CSharpClientException : Exception
+    {
+        public CSharpClientException() { }
+        public CSharpClientException(string message) : base(message) { }
+        public CSharpClientException(string message, Exception inner) : base(message, inner) { }
+        protected CSharpClientException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        { }
+    }
+
+    public class JsonMessage : CSharpServerClientBaseMessage
+    {
+        public dynamic Result { get; set; }
+    }
+}
